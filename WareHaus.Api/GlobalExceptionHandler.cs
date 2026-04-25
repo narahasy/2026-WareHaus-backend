fe1553db8827bbc8fe1f4e175ef561a3aebcc7ec
Nabilah Nuraini Rohadatul 'Aisy <nabilahnuraisy@gmail.com>
2026-04-26 00:16:48 +0700

    add GEH and update program.cs

--- WareHaus.Api/GlobalExceptionHandler.cs
+++ WareHaus.Api/GlobalExceptionHandler.cs
@@ -1,24 +1,59 @@
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WareHaus.Api;
 
 public class GlobalExceptionHandler : IExceptionHandler
 {
+    private readonly IProblemDetailsService _problemDetailsService;
+
+    public GlobalExceptionHandler(IProblemDetailsService problemDetailsService)
+    {
+        _problemDetailsService = problemDetailsService;
+    }
+
     public async ValueTask<bool> TryHandleAsync(
         HttpContext httpContext,
         Exception exception,
         CancellationToken cancellationToken)
     {
-        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        var statusCode = StatusCodes.Status500InternalServerError;
+        var title = "Internal Server Error";
+        var detail = exception.Message;
+        IEnumerable<string> customErrors = new[] { exception.Message };
 
-        var response = new
+        switch (exception)
         {
-            Title = "Server Error",
-            Message = exception.Message
+            case KeyNotFoundException:
+                statusCode = StatusCodes.Status404NotFound;
+                title = "Resource Not Found";
+                break;
+            case ArgumentException:
+                statusCode = StatusCodes.Status400BadRequest;
+                title = "Invalid Request";
+                break;
+            case UnauthorizedAccessException:
+                statusCode = StatusCodes.Status401Unauthorized;
+                title = "Unauthorized Access";
+                break;
+        }
+
+        httpContext.Response.StatusCode = statusCode;
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = title,
+            Detail = detail
         };
 
-        await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
+        problemDetails.Extensions["errors"] = customErrors;
 
-        return true;
+        return await _problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            ProblemDetails = problemDetails,
+            Exception = exception
+        });
     }
 }
