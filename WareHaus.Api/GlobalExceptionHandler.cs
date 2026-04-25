21c6bd034558dbf7b12979bfc156470a5aaf3b17
Nabilah Nuraini Rohadatul 'Aisy <nabilahnuraisy@gmail.com>
2026-04-26 00:19:00 +0700

    update GlobalExceptionHandler.cs

--- WareHaus.Api/GlobalExceptionHandler.cs
+++ WareHaus.Api/GlobalExceptionHandler.cs
@@ -1,1 +1,59 @@
-G
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WareHaus.Api;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly IProblemDetailsService _problemDetailsService;
+
+    public GlobalExceptionHandler(IProblemDetailsService problemDetailsService)
+    {
+        _problemDetailsService = problemDetailsService;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var statusCode = StatusCodes.Status500InternalServerError;
+        var title = "Internal Server Error";
+        var detail = exception.Message;
+        IEnumerable<string> customErrors = new[] { exception.Message };
+
+        switch (exception)
+        {
+            case KeyNotFoundException:
+                statusCode = StatusCodes.Status404NotFound;
+                title = "Resource Not Found";
+                break;
+            case ArgumentException:
+                statusCode = StatusCodes.Status400BadRequest;
+                title = "Invalid Request";
+                break;
+            case UnauthorizedAccessException:
+                statusCode = StatusCodes.Status401Unauthorized;
+                title = "Unauthorized Access";
+                break;
+        }
+
+        httpContext.Response.StatusCode = statusCode;
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = title,
+            Detail = detail
+        };
+
+        problemDetails.Extensions["errors"] = customErrors;
+
+        return await _problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            ProblemDetails = problemDetails,
+            Exception = exception
+        });
+    }
+}
